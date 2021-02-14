ec599f26290bfc7517e885227f4a020547e7ef47
mikebernal <mikebernal.me@gmail.com>
2021-02-15 01:52:29 +0800

    feat: toast component

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,12 @@ import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import Link from 'next/link'
 import Loader from '../components/Loader'
+import toast from 'react-hot-toast'
 
 export default function Home() {
   return (
     <div>
-      <Loader show />
+      <button onClick={() => toast.success('Hello toast!')}>TOAST ME</button>
     </div>
   )
 }
