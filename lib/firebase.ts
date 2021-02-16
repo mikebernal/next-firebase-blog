6e8d7dd1b0d4aba6962661108625f78f9d900b35
mikebernal <mikebernal.me@gmail.com>
2021-02-17 03:57:39 +0800

    feat: edit form

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,51 +1,58 @@
-import fb from 'firebase/app'
-import 'firebase/auth'
-import 'firebase/firestore'
-import 'firebase/storage'
+import firebase from 'firebase/app';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDDyeaAikom0brcqR_Z54_6zpHFmWqFOqs",
-    authDomain: "mbernal-nextfire.firebaseapp.com",
-    projectId: "mbernal-nextfire",
-    storageBucket: "mbernal-nextfire.appspot.com",
-    messagingSenderId: "338776360766",
-    appId: "1:338776360766:web:e273baf194d4a6e18c8e6f"
+  apiKey: 'AIzaSyBX5gkKsbOr1V0zxBuSqHWFct12dFOsQHA',
+  authDomain: 'nextfire-demo.firebaseapp.com',
+  projectId: 'nextfire-demo',
+  storageBucket: 'nextfire-demo.appspot.com',
+  messagingSenderId: '827402452263',
+  appId: '1:827402452263:web:c9a4bea701665ddf15fd02',
 };
 
-// Initialize app
-export const firebase = !fb.apps.length ? fb.initializeApp(firebaseConfig) : fb.app()
-
-export const auth = fb.auth()
-
-export const googleAuthProvider = new fb.auth.GoogleAuthProvider()
-
-export const firestore = fb.firestore()
-export const storage   = fb.storage()
-
-// Helper functions
-
-/**
- * Gets a users/{uid} document with username
- * @param {string} username 
- */
-export async function getUserWithUsername(username) {
-    const usersRef = firestore.collection('users')
-    const query    = usersRef.where('username', '==', username).limit(1)
-    const userDoc  = (await query.get()).docs[0]
-    return userDoc
+if (!firebase.apps.length) {
+  firebase.initializeApp(firebaseConfig);
 }
 
-/**
- * Convertsa firestore document to JSON
- * @param {DocumentSnapshot} doc 
+// Auth exports
+export const auth = firebase.auth();
+export const googleAuthProvider = new firebase.auth.GoogleAuthProvider();
+
+// Firestore exports
+export const firestore = firebase.firestore();
+export const serverTimestamp = firebase.firestore.FieldValue.serverTimestamp;
+export const fromMillis = firebase.firestore.Timestamp.fromMillis;
+export const increment = firebase.firestore.FieldValue.increment;
+
+// Storage exports
+export const storage = firebase.storage();
+export const STATE_CHANGED = firebase.storage.TaskEvent.STATE_CHANGED;
+
+/// Helper functions
+
+/**`
+ * Gets a users/{uid} document with username
+ * @param  {string} username
+ */
+export async function getUserWithUsername(username) {
+  const usersRef = firestore.collection('users');
+  const query = usersRef.where('username', '==', username).limit(1);
+  const userDoc = (await query.get()).docs[0];
+  return userDoc;
+}
+
+/**`
+ * Converts a firestore document to JSON
+ * @param  {DocumentSnapshot} doc
  */
 export function postToJSON(doc) {
-    const data = doc.data()
-    
-    return {
-        ...data,
-        // Gotcha! firestore timestamp NOT serializable to JSON
-        createdAt: data.createdAt.toMillis(),
-        updatedAt: data.updatedAt.toMillis(),
-    }
+  const data = doc.data();
+  return {
+    ...data,
+    // Gotcha! firestore timestamp NOT serializable to JSON. Must convert to milliseconds
+    createdAt: data?.createdAt.toMillis() || 0,
+    updatedAt: data?.updatedAt.toMillis() || 0,
+  };
 }
--- pages/[username]/[slug].tsx
+++ pages/[username]/[slug].tsx
@@ -1,68 +1,93 @@
-import styles from '../../styles/Post.module.css'
-import PostContent from '../../components/PostContent'
-import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase'
-import { useDocumentData } from 'react-firebase-hooks/firestore' 
+import styles from '../../styles/Post.module.css';
+import PostContent from '../../components/PostContent';
+import HeartButton from '../../components/HeartButton';
+import AuthCheck from '../../components/AuthCheck';
+import Metatags from '../../components/Metatags';
+import { UserContext } from '../../lib/context';
+import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
 
-export default function Post(props) {
-    const postRef = firestore.doc(props.path)
-    const [realtimePost] = useDocumentData(postRef)
+import Link from 'next/link';
+import { useDocumentData } from 'react-firebase-hooks/firestore';
+import { useContext } from 'react';
 
-    const post = realtimePost || props.post
+export async function getStaticProps({ params }) {
+  const { username, slug } = params;
+  const userDoc = await getUserWithUsername(username);
 
-    return (
-        <main className={styles.container}>
+  let post;
+  let path;
 
-            <section>
-                <PostContent post={post} />
-            </section>
+  if (userDoc) {
+    const postRef = userDoc.ref.collection('posts').doc(slug);
+    post = postToJSON(await postRef.get());
 
-            <aside className="card">
-                <p>
-                    <strong>{post.heartCount || 0} ❤️</strong>
-                </p>
-            </aside>
+    path = postRef.path;
+  }
 
-        </main>
-    )
+  return {
+    props: { post, path },
+    revalidate: 100,
+  };
 }
 
 export async function getStaticPaths() {
-    const snapshot = await firestore.collectionGroup('posts').get()
+  // Improve my using Admin SDK to select empty docs
+  const snapshot = await firestore.collectionGroup('posts').get();
 
-    const paths = snapshot.docs.map((doc) => {
-        const { slug, username } = doc.data()
-        return {
-            params: { username, slug },
-        }
-    })
-
+  const paths = snapshot.docs.map((doc) => {
+    const { slug, username } = doc.data();
     return {
-        paths,
-        isFallback: 'blocking'
-    }
+      params: { username, slug },
+    };
+  });
 
-
+  return {
+    // must be in this format:
+    // paths: [
+    //   { params: { username, slug }}
+    // ],
+    paths,
+    fallback: 'blocking',
+  };
 }
 
-export async function getStaticProps({ params }) {
-    const { username, slug } = params
-    const userDoc = await getUserWithUsername(username)
+export default function Post(props) {
+  const postRef = firestore.doc(props.path);
+  const [realtimePost] = useDocumentData(postRef);
 
-    let post
-    let path
+  const post = realtimePost || props.post;
 
-    if (userDoc) {
-        const postRef = userDoc.ref.collection('posts').doc(slug)
-        post = postToJSON(await postRef.get())
+  const { user: currentUser } = useContext(UserContext);
 
-        path = postRef.path
-    }
+  return (
+    <main className={styles.container}>
+      <Metatags title={post.title} description={post.title} />
+      
+      <section>
+        <PostContent post={post} />
+      </section>
 
-    return {
-        props: {
-            post,
-            path,
-        },
-        revalidate: 5000,
-    }
+      <aside className="card">
+        <p>
+          <strong>{post.heartCount || 0} 🤍</strong>
+        </p>
+
+        <AuthCheck
+          fallback={
+            <Link href="/enter">
+              <button>💗 Sign Up</button>
+            </Link>
+          }
+        >
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
+
+        {currentUser?.uid === post.uid && (
+          <Link href={`/admin/${post.slug}`}>
+            <button className="btn-blue">Edit Post</button>
+          </Link>
+        )}
+      </aside>
+    </main>
+  );
 }
